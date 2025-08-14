5160551212eccbf6a0aa6a0a0098eb9659354046
Mohanapriya2828 <mohanapriya.jayabalan@psiog.com>
2025-08-14 14:33:48 +0530

    First commit

---
+++ main.cpp
@@ -0,0 +1,29 @@
+int main() {
+    string line;
+
+    if (!fileExists("employee_details.csv")) {
+        cerr << "Error: employee_details.csv does not exist!" << endl;
+        return 1;
+    }
+    if(fileEmpty("employee_details.csv"))
+    {
+        cout<<"employee_details.csv is empty"<<endl;
+        return 0;
+    }
+
+    
+    if (fileExists("employee_details_copy.csv")) {
+        if (!fileEmpty("employee_details_copy.csv")) {
+            
+            remove("employee_details_copy.csv");
+            fstream bFile("employee_details_copy.csv", ios::in | ios::out | ios::trunc);
+            bFile.close();
+            cout << "Cleared existing data from employee_details_copy.csv" << endl;
+        }
+    } else {
+        
+        fstream bFile("employee_details_copy.csv", ios::in | ios::out | ios::trunc);
+        bFile.close();
+        cout << "Created new employee_details_copy.csv" << endl;
+    }
+}
